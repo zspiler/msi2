bb23ba4bd39fb0a0f486213418e624931ee7b895
zanspiler <zanspiler1@gmail.com>
2020-12-19 20:16:10 +0100

    Connected Flash to MongoDB

--- app.py
+++ app.py
@@ -1,40 +1,25 @@
+import sys
 from flask import Flask, request, render_template
-app = Flask(__name__)
+from pymongo import MongoClient
 
-x = 42
+app = Flask(__name__)
+client = MongoClient("mongodb://db:27017")
+db = client.test_db
+
+
+counter = db.find_one()
+if counter == None:
+    db.insert_one({ 'counter' : 1 })
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    global x
+
+    c = db.find_one()
+
+
     if request.method == 'POST':
-        x += 1
-    return render_template('index.html', counter=x) 
+        pass
+    return render_template('index.html', counter=42) 
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
-
-
-
-# import time
-
-# import redis
-# from flask import Flask
-
-# app = Flask(__name__)
-# cache = redis.Redis(host='redis', port=6379)
-
-# def get_hit_count():
-#     retries = 5
-#     while True:
-#         try:
-#             return cache.incr('hits')
-#         except redis.exceptions.ConnectionError as exc:
-#             if retries == 0:
-#                 raise exc
-#             retries -= 1
-#             time.sleep(0.5)
-
-# @app.route('/')
-# def hello():
-#     count = get_hit_count()
-#     return 'Hello World! I have been seen {} times.\n'.format(count)
