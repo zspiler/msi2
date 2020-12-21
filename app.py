f20c6be12e4dad0a9357021a35ee584d70ef0ad0
zanspiler <zanspiler1@gmail.com>
2020-12-21 17:58:51 +0100

    added nginx reverse proxy

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 client = MongoClient("mongodb://db:27017")
 db = client.test_db
 
-
 counter = db.col.find_one()
 if counter == None:
     db.col.insert_one({ 'counter' : 1 })
@@ -13,13 +12,12 @@ if counter == None:
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    counter = db.col.find_one()['counter']
- 
+    counter = db.col.find_one()['counter'] 
     if request.method == 'POST':
         counter += 1
         db.col.find_one_and_update({}, {'$set': { 'counter': counter }})
         
-    return render_template('index.html', counter=counter) 
+    return render_template('index.html', x=counter) 
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
