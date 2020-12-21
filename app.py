a64d937aa2d804ddda0b9524122bc696d5e9e3bb
zanspiler <zanspiler1@gmail.com>
2020-12-21 19:40:59 +0100

    Added redis for storing users' clicks on the container

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from flask import Flask, request, render_template
 from pymongo import MongoClient
+from redis import Redis 
 
 app = Flask(__name__)
-client = MongoClient("mongodb://db:27017")
-db = client.test_db
+db = MongoClient("mongodb://db:27017").test_db
+redis = Redis(host='redis', port=6379)
+
 
 counter = db.col.find_one()
 if counter == None:
     db.col.insert_one({ 'counter' : 1 })
+
+if redis.get('hits') == None:
+    redis.incr('hits')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     counter = db.col.find_one()['counter'] 
     if request.method == 'POST':
+        redis.incr('hits')
         counter += 1
         db.col.find_one_and_update({}, {'$set': { 'counter': counter }})
-        
-    return render_template('index.html', x=counter) 
+    return render_template('index.html', x=counter, hits=int(redis.get('hits'))) 
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
+
+# cat pictures?
