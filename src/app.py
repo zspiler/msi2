8465b54ff6752cf4a94b954475dc41e3289fe4f6
zanspiler <zanspiler1@gmail.com>
2020-12-30 19:53:29 +0100

    added Redis caching

--- src/app.py
+++ src/app.py
@@ -1,30 +1,23 @@
 from flask import Flask, request, render_template
 from pymongo import MongoClient
-from redis import Redis 
+from flask_caching import Cache
 
 app = Flask(__name__)
 db = MongoClient("mongodb://db:27017").test_db
-redis = Redis(host='redis', port=6379)
-
+cache = Cache(app, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': 'redis://redis:6379/0'})
 
 counter = db.col.find_one()
 if counter == None:
     db.col.insert_one({ 'counter' : 1 })
 
-if redis.get('hits') == None:
-    redis.incr('hits')
-
-
 @app.route('/', methods=['GET', 'POST'])
+@cache.cached(timeout=5)
 def main():
     counter = db.col.find_one()['counter'] 
     if request.method == 'POST':
-        redis.incr('hits')
         counter += 1
         db.col.find_one_and_update({}, {'$set': { 'counter': counter }})
-    return render_template('index.html', x=counter, hits=int(redis.get('hits'))) 
+    return render_template('index.html', x=counter) 
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
-
-# cat pictures?
